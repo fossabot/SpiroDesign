1450afc1f9462114da9787f191ed5a4ef657b5d4
Tosachi <tscmgsn.42@gmail.com>
2019-06-28 15:58:35 +0900

    右クリックを検出

--- spirogear/SpiroController.java
+++ spirogear/SpiroController.java
@@ -4,6 +4,13 @@ import spirogear.MVC.mvc.View;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import javax.swing.event.MouseInputAdapter;
 
 public class SpiroController extends Controller implements ActionListener {
 
@@ -23,8 +30,22 @@ public class SpiroController extends Controller implements ActionListener {
 		return null;
 	}
 
+	/**
+	 * 右クリックが行われた際、その座標を獲得してその位置にメニューを表示するようViewに依頼する。
+	 * @param aMouseEvent
+	 */
 	public void mouseClicked(MouseEvent aMouseEvent) {
+		int btn = aMouseEvent.getButton();
 
+  if (btn == MouseEvent.BUTTON1){
+		// 左クリックの処理
+		
+  }else if (btn == MouseEvent.BUTTON3){
+		Point aPoint = aMouseEvent.getPoint();
+		this.showPopupMenu(aPoint, aMouseEvent);
+		System.out.println("右クリック");
+		System.out.println(aPoint);
+  	}
 	}
 
 	public void mouseDragged(MouseEvent aMouseEvent) {
@@ -43,8 +64,9 @@ public class SpiroController extends Controller implements ActionListener {
 
 	}
 
-	public void showPopupMenu(MouseEvent aMouseEvent) {
+	public void showPopupMenu(Point aPoint, MouseEvent aMouseEvent) {
 
+		return;
 	}
 
 	public String toString() {
